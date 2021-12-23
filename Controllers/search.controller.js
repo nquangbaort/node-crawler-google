90f67d0fc93fdb0a23ad362a8fc13742b536bb2e
nquangbaort <baonq@vaixgroup.com>
2021-12-23 21:26:04 +0700

    update

--- Controllers/search.controller.js
+++ Controllers/search.controller.js
@@ -1,20 +1,7 @@
 const {stringify} = require('csv-stringify');
-let request = require('request');
-const cheerio = require("cheerio");
-request = request.defaults({jar: true});
-const fs = require('fs');
-let ip = []
-let userGents = []
-let proxys = []
-fs.readFile('./user-agents/ip.txt', {encoding:'utf8', flag:'r'}, function(err, data) {
-    if(err) throw err
-    proxys = data.split(/\r\n/)
-});
-// fs.readFile('./user-agents/user_agents.txt', {encoding:'utf8', flag:'r'}, function(err, data) {
-//     if(err) throw err
-//     userGents = data.split(/\r\n/)
-// });
-
+const fs = require('fs')
+var path = require('path');
+require('dotenv').config();
 let columns = {
     title: 'title',
     link: 'link'
@@ -22,42 +9,36 @@ let columns = {
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-module.exports.index = async (req, res, next) => {
-    const query = req.query
-    if(query.q) {
-        let data = []
-        let nameFileCSV = `data_search-${query.q}-${Math.floor(Math.random()*999999)}.csv`
-        for(let i = 1; i <= 1000 ; i+=10){
-             const options = {
-                url: 'www.google.com',
-                // headers: {
-                //     'User-Agent': userGents[Math.floor(Math.random()*userGents.length)]
-                // },
-                proxy: proxys[Math.floor(Math.random()*proxys.length)]
-            };
-            request(options, function () {
-                request(encodeURI(`https://www.google.com/search?q=${query.q}&start=${i}`), function (error, response, body) {
-                    if(error) throw error;
-                    var $ = cheerio.load(body);
-                    console.log(body)
-                    $('h3').each((index , elem) => {
-                        const title = $(elem).text();
-                        const link = $(elem).parent().attr('href');
-                        data.push({ title, link }); 
-                        console.log(data)
-                        stringify(data, { header: true, columns: columns }, (err, output) => {
-                            if (err) throw err;
-                            fs.writeFile(nameFileCSV, output, (err) => {
-                                if (err) throw err;
-                                console.log(`${nameFileCSV} saved`);
-                            });
-                        });
-                    })
+const SERP_API = require('google-search-results-nodejs');
+const SEARCH_API = new SERP_API.GoogleSearch(process.env.API_KEY);
+let dataContent = []
+module.exports.search = (req , res , next) => {
+    let contentFie = []
+    let q = req.query.q
+    let nameFileCSV = `data_search-${q}-${Math.floor(Math.random()*999999)}.csv`
+    let dataSearch = []
+    for(let i = 1; i <= 100 ; i+=10){
+        const params = {
+            engine: "google",
+            q: q,
+            num : 100,
+            start : i
+        };
+        SEARCH_API.json(params,  (data) => {
+            dataSearch = data['organic_results']
+            dataContent.push(data['organic_results'])
+            for(let i = 0; i< dataSearch.length; i++){
+                let title = dataSearch[i].title
+                let link = dataSearch[i].link
+                contentFie.push({ title, link });
+            }
+            stringify(contentFie, { header: true, columns: columns }, (err, output) => {
+                if (err) throw err;
+                fs.writeFile(path.resolve()+'/public/csv/'+nameFileCSV, output, (err) => {
+                    if (err) throw err;
                 });
             });
-           await sleep(1000);
-        }
+        });
     }
-    
-    res.render('search', { title: 'Custom search api google' });
+    res.render('search', { title: 'Custom search api google' , dataSearch : dataSearch , file : process.env.BASE_URL_CSV+nameFileCSV});
 }
--- routes/search.js
+++ routes/search.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 const searchController = require('../Controllers/search.controller')
 /* GET users listing. */
-router.get('/', searchController.index);
+router.get('/', searchController.search);
 
 module.exports = router;
